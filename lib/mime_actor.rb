6b2dea3936717042486f89cd97f5e79984e2a9e7
Ryan Chang <ryancyq@gmail.com>
2024-06-22 03:54:49 +0800

    lib: allow including MimeActor to include Formatter together

--- lib/mime_actor.rb
+++ lib/mime_actor.rb
@@ -5,7 +5,9 @@ require_relative "mime_actor/version"
 require 'active_support'
 
 module MimeActor
+  extend ActiveSupport::Concern
   extend ActiveSupport::Autoload
 
   autoload :Formatter
+  include Formatter
 end
