6209aa73af73f0458a36803dcd6861711ada4ded
Ryan Chang <ryancyq@gmail.com>
2024-06-21 17:57:34 +0800

    Initial commit

---
+++ lib/mime_actor.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mime_actor/version"
+
+module MimeActor
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mime_actor/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MimeActor
+  VERSION = "0.1.0"
+end
