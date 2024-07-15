d2c0de62f1b5dce5c6fde4ed9bce28ce85f26895
Ryan Chang <ryancyq@gmail.com>
2024-07-15 23:56:19 +0800

    lib: add action and format to callbacks

--- lib/mime_actor/callbacks.rb
+++ lib/mime_actor/callbacks.rb
@@ -27,20 +27,40 @@ module MimeActor
     end
 
     module ClassMethods
-      # Defines a callback that will get called right before the act is cued.
-      #
-      # @param block the `block` to evaluate when `with` is not provided
-      #
-      def before_act(*filters, &)
-        set_callback(:act, :before, *filters, &)
+      %i[before after around].each { |kind| define_act_callbacks(kind) }
+
+      def configure_act_filters
       end
 
-      def after_act(*filters, &)
-        set_callback(:act, :after, *filters, &)
-      end
+      private
 
-      def around_act(*filters, &)
-        set_callback(:act, :around, *filters, &)
+      def define_act_callbacks(kind)
+        module_eval(
+          # def before_act
+          #   configure_act_filters(callbacks, action, format, block) do |callback, options|
+          #     set_callback(:act, kind, callback, options)
+          #   end
+          # end
+          #
+          # def prepend_before_act
+          #   configure_act_filters(callbacks, action, format, block) do |callback, options|
+          #     set_callback(:act, kind, callback, options.merge!(prepend: true))
+          #   end
+          # end
+          <<-RUBY, __FILE__, __LINE__ + 1
+            def self.#{kind}_act(*callbacks, action: nil, format: nil, &block)
+              configure_act_filters(callbacks, action, format, block) do |name, options|
+                set_callback(:act, kind, name, options)
+              end
+            end
+
+            def self.prepend_#{kind}_act(*callbacks, action: nil, format: nil, &block)
+              configure_act_filters(callbacks, action, format, block) do |callback, options|
+                set_callback(:act, kind, callback, options.merge!(prepend: true))
+              end
+            end
+          RUBY
+        )
       end
     end
   end
