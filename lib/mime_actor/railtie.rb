238ebc9e10e2690e3edafaf8f348854ad563fce5
Ryan Chang <ryancyq@gmail.com>
2024-07-21 15:00:27 +0800

    lib: add deprecator to railtie

--- lib/mime_actor/railtie.rb
+++ lib/mime_actor/railtie.rb
@@ -4,5 +4,8 @@
 
 module MimeActor
   class Railtie < Rails::Railtie
+    initializer "mime_actor.deprecator", before: :load_environment_config do |app|
+      app.deprecators[:mime_actor] = MimeActor.deprecator
+    end
   end
 end
