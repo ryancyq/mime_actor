5c7d930abf997e9bb9860f0b39bf8c2455bdf3dc
Ryan Chang <ryancyq@gmail.com>
2024-07-23 21:29:07 +0800

    ci: allow running codecov report using CODE_COV ENV

--- spec/support/simplecov.rb
+++ spec/support/simplecov.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
-if ENV["CI"]
+if ENV["CI"] || ENV["CODE_COV"]
   require "simplecov"
-  require "simplecov-cobertura"
   SimpleCov.start
+end
+
+if ENV["CI"]
+  require "simplecov-cobertura"
   SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
 end
