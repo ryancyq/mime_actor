1668195af590bd1e7a3f3c7569ee85ada62e57b4
Ryan Chang <ryancyq@gmail.com>
2024-07-26 22:44:12 +0800

    ci: make test coverage opt-out by default

--- spec/support/simplecov.rb
+++ spec/support/simplecov.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-if ENV["CI"] || ENV["CODE_COV"]
+if ENV["CODE_COV"]
   require "simplecov"
   SimpleCov.start
 end
 
-if ENV["CI"]
+if ENV["CODE_COV"] && ENV["CI"]
   require "simplecov-cobertura"
   SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
 end
