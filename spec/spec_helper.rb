211b513bb7ac543192f3ed0f17a9fc87bd14647a
Ryan Chang <ryancyq@gmail.com>
2024-07-27 21:01:44 +0800

    spec: configure rspec recommended settings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,12 @@
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+  
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
