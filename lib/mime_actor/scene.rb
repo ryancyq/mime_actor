a21d3560ee1258530764856f47a358ced4fb645f
Ryan Chang <ryancyq@gmail.com>
2024-07-30 12:14:58 +0800

    lib: replace #respond_act_to with #act_on_action with ActiveSuport::CodeGenerator for action + format registration

--- lib/mime_actor/scene.rb
+++ lib/mime_actor/scene.rb
@@ -5,10 +5,9 @@
 require "mime_actor/errors"
 require "mime_actor/validator"
 
+require "active_support/code_generator"
 require "active_support/concern"
-require "active_support/core_ext/array/wrap"
 require "active_support/core_ext/module/attribute_accessors"
-require "active_support/core_ext/object/blank"
 
 module MimeActor
   # # MimeActor Scene
@@ -98,7 +97,78 @@ module MimeActor
         end
       end
 
+      # Register `action` + `format` definitions.
+      #
+      # @param action a collection of `action`
+      # @param format a single `format` or a collection of `format`
+      # @param with the response handler for the given `action` + `format`
+      # @param block the `block` to be yieled for the given `action` + `format`
+      #
+      # @example register a `html` format on action `create`
+      #   act_on_action :create, format: :html
+      #
+      #   # an action method will be defined in the class
+      #   def indexl; end
+      # @example register `html`, `json` formats on actions `index`, `show`
+      #   act_on_action :index, :show, format: [:html, :json]
+      #
+      #   # these action methods will be defined in the class
+      #   def index; end
+      #   def show; end
+      # @example register a `html` format on action `index` with response handler method
+      #   act_on_action :index, format: :html, with: :render_html
+      #
+      #   # an action method will be defined in the class
+      #   def index; end
+      #   # the given method should be defined in the class
+      #   def render_html; end
+      # @example register a `html` format on action `index` with response handler block
+      #   act_on_action :html, on: :index do
+      #     render :index
+      #   end
+      #
+      #   # an action method will be defined in the class
+      #   def index; end
+      #
+      # For each unique `action` being registered, a corresponding `action` method will be defined.
+      def act_on_action(*actions, format:, with: nil, &block)
+        raise ArgumentError, "format is required" if format.nil?
+        raise ArgumentError, "provide either with: or a block" if !with.nil? && block_given?
+
+        validate!(:actions, actions)
+        validate!(:format_or_formats, format)
+        validate!(:callable, with) unless with.nil?
+        with = block if block_given?
+
+        actions.each do |action|
+          acting_scenes[action] ||= {}
+          Array(format).each do |action_format|
+            acting_scenes[action][action_format] = with
+          end
+        end
+
+        generate_action_methods(actions)
+      end
+
       private
+
+      def generated_action_methods
+        @generated_action_methods ||= Module.new.tap { |mod| prepend mod }
+      end
+
+      def generate_action_methods(actions)
+        ActiveSupport::CodeGenerator.batch(generated_action_methods, __FILE__, __LINE__) do |owner|
+          actions.each do |action|
+            owner.define_cached_method(action, namespace: :mime_scene) do |batch|
+              batch.push(
+                "def #{action}",
+                "respond_to?(:start_scene) ? start_scene { super } : super",
+                "end"
+              )
+            end
+          end
+        end
+      end
 
       def compose_scene(action, format, actor)
         action_defined = (instance_methods + private_instance_methods).include?(action.to_sym)
